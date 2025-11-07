adc340c26a20aa20109cd0480b6c728b241e2906
utsahice <utsahic.encircle@gmail.com>
2025-11-07 16:50:35 +0530

    fix : draft mode enable

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,14 +1,23 @@
-// app/api/draft
+// app/api/draft/route.ts
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
-  const slug = searchParams.get("slug");
+  const slug = searchParams.get("slug") || "home";
 
   const draft = await draftMode();
   draft.enable();
 
-  redirect(`/${slug}?${searchParams.toString()}`);
+  const params = new URLSearchParams(searchParams);
+
+  if (!params.has("_storyblok")) {
+    params.append("_storyblok", "1");
+  }
+
+  const queryString = params.toString();
+  const redirectUrl = `/${slug}?${queryString}`;
+
+  redirect(redirectUrl);
 };
